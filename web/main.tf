8f6a26681eb49daae00f9fd10cd42809f3a5ee0d
VigneshCPandian <69304288+VigneshCPandian@users.noreply.github.com>
2024-07-02 22:00:41 +0100

    sg updated

--- web/main.tf
+++ web/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "server" {
   ami           = "ami-04b70fa74e45c3917" 
   instance_type = "t2.micro"
   subnet_id     = var.sn
-  security_groups = var.sg
+  security_groups = [var.sg]
 
   tags = {
     Name = "myserver"
